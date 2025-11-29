225ce52dacd8b78b0091086431a1c36f6d477c74
Ivan Mutavdzic <imutavdz@f0r4s8.codam.nl>
2025-11-29 17:14:13 +0100

    prototype

--- include/lexer.h
+++ include/lexer.h
@@ -6,7 +6,7 @@
 /*   By: imutavdz <imutavdz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 18:50:31 by imutavdz          #+#    #+#             */
-/*   Updated: 2025/11/28 20:01:58 by imutavdz         ###   ########.fr       */
+/*   Updated: 2025/11/29 16:58:08 by imutavdz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LEXER_H
@@ -14,12 +14,25 @@
 
 #include "token.h"
 
-typedef struct s_env
-{
-	char	**envp;
-	int		count;
-	int		capacity;
-}	t_env;
+// typedef enum e_char_class
+// {
+// 	C_WS,
+// 	C_Q,
+// 	C_P,
+// 	C_R,
+// 	C_W,
+// 	C_V,
+// 	c_OP,
+// 	C_EOF
+
+// }	t_char_class;
+
+// typedef struct s_env
+// {
+// 	char	**envp;
+// 	int		count;
+// 	int		capacity;
+// }	t_env;
 
 //curr state of the scanner
 typedef struct s_scanner
@@ -34,22 +47,23 @@ typedef struct s_scanner
 }	t_scanner;
 
 
-t_token	*lexer(char *input_text);
-t_token	*scan_next_tok(t_scanner *scanner);
-t_token	*scan_word(t_scanner *scanner);
-t_token *scan_var(t_scanner *scanner);
-t_token	*scan_operator(t_scanner *scanner);
-t_token *scan_quoted_str(t_scanner *scanner);
-void	init_scanner(t_scanner *scanner, char *input_text);
+t_token			*lexer(char *input_text);
+t_token			*scan_next_tok(t_scanner *scanner);
+t_token			*scan_word(t_scanner *scanner);
+t_token 		*scan_var(t_scanner *scanner);
+t_token			*scan_operator(t_scanner *scanner);
+t_token 		*scan_quoted_str(t_scanner *scanner);
+void			init_scanner(t_scanner *scanner, char *input_text);
 
-t_scan_state	sngl_quote(t_scanner *scanner);
-t_scan_state	dbl_quote(t_scanner *scanner);
+static int		chop_word(t_scan_state state, char c);
+static int		handle_qt_switch(t_scan_state *state, t_scanner *scanner, char c);
+t_scan_state	switch_state(t_scan_state curr, char c);
 
-void	append_char(t_scanner *scanner, char c);
-char	*get_buff_lexeme(t_scanner *scanner);
-void	advance(t_scanner *scanner);
-char	peek(t_scanner *scanner);
-char	peek_next(t_scanner *scanner);
+void			append_char(t_scanner *scanner, char c);
+char			*get_buff_lexeme(t_scanner *scanner);
+void			advance(t_scanner *scanner);
+char			peek(t_scanner *scanner);
+char			peek_next(t_scanner *scanner);
 
 
 #endif
