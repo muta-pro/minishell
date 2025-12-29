689c578d14ff26e047e8b63253c0f3699c5869c6
Yoan Neshev <jokosan97@gmail.com>
2025-12-29 17:46:57 +0100

    fixed exit leaks and exiting when it shouldnt

--- src/exit.c
+++ src/exit.c
@@ -6,7 +6,7 @@
 /*   By: joko <joko@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/12/26 16:14:30 by joko          #+#    #+#                 */
-/*   Updated: 2025/12/29 16:57:29 by yneshev       ########   odam.nl         */
+/*   Updated: 2025/12/29 17:46:36 by yneshev       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,13 +42,13 @@ int	ft_exit(t_ast_node *cmd, t_shell *shell)
 			write(STDERR_FILENO, "minishell: exit: ", 17);
 			write(STDERR_FILENO, cmd->args[1], ft_strlen(cmd->args[1]));
 			write(STDERR_FILENO, ": numeric argument required\n", 28);
-			exit(2);
+			shell->exit_status = 2;
+			return (-42);
 		}
 		if (cmd->args[2])
 		{
 			write(STDERR_FILENO, "minishell: exit: too many arguments\n", 36);
-			// shell->exit_status = 1;
-			// return (-42);
+			return (1);
 		}
 		shell->exit_status = atoi(cmd->args[1]);
 		return (-42);
