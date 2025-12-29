b57d481a0968b36011a5d60e4e08c121dd41482a
Yoan Neshev <jokosan97@gmail.com>
2025-12-29 17:38:19 +0100

    fixed redir.c norm

--- src/redir.c
+++ src/redir.c
@@ -6,44 +6,60 @@
 /*   By: yneshev <yneshev@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/12/07 15:49:06 by yneshev       #+#    #+#                 */
-/*   Updated: 2025/12/29 17:21:57 by yneshev       ########   odam.nl         */
+/*   Updated: 2025/12/29 17:36:02 by yneshev       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-int	apply_redir(t_redir *node)
+static int	redir_out_append(t_redir *node)
 {
 	int	fd;
 
-    while (node)
-    {
-        if (node->type == T_REDIR_OUT || node->type == T_REDIR_APPEND)
-        {
-            if (node->type == T_REDIR_OUT)
-                fd = open(node->file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            else
-                fd = open(node->file_name, O_WRONLY | O_CREAT | O_APPEND, 0644);
-            if (fd == -1)
-            {
-                perror(node->file_name);
-                return (-1);
-            }
-            dup2(fd, STDOUT_FILENO);
-            close(fd);
-        }
-        else if (node->type == T_REDIR_IN)
-        {
-            fd = open(node->file_name, O_RDONLY);
-            if (fd == -1)
-            {
-                perror(node->file_name);
-                return (-1);
-            }
-            dup2(fd, STDIN_FILENO);
-            close(fd);
-        }
-        node = node->next;
-    }
-    return (0);
+	if (node->type == T_REDIR_OUT)
+		fd = open(node->file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else
+		fd = open(node->file_name, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (fd == -1)
+	{
+		perror(node->file_name);
+		return (-1);
+	}
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+	return (0);
+}
+
+static int	redir_in(t_redir *node)
+{
+	int	fd;
+
+	fd = open(node->file_name, O_RDONLY);
+	if (fd == -1)
+	{
+		perror(node->file_name);
+		return (-1);
+	}
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	return (0);
+}
+
+int	apply_redir(t_redir *node)
+{
+	while (node)
+	{
+		if (node->type == T_REDIR_OUT || node->type == T_REDIR_APPEND)
+		{
+			if (redir_out_append(node) == -1)
+				return (-1);
+		}
+		else if (node->type == T_REDIR_IN)
+		{
+			if (redir_in(node) == -1)
+				return (-1);
+		}
+		node = node->next;
+	}
+	return (0);
 }
